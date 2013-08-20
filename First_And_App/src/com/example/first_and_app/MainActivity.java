08c048a29b95fe01c2fac0e1c01cfb8a1844bf56
FanaticFiz <fanatic83@gmail.com>
2013-08-20 15:32:14 +0300

    Добавил второй активити

--- First_And_App/src/com/example/first_and_app/MainActivity.java
+++ First_And_App/src/com/example/first_and_app/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.first_and_app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button but1 = (Button) findViewById(R.id.button1);
+		but1.setOnClickListener(new OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				// TODO Auto-generated method stub
+				Intent intent1 = new Intent(MainActivity.this, Second.class);
+				startActivity(intent1);
+			}
+		});
 	}
 
 	@Override
